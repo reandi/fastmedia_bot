38438a0e50cba8f69d42937c1976622a37698bb4
reandi <44109370+reandi@users.noreply.github.com>
2026-02-28 19:01:51 +0700

    Fastmedia_bot.py

--- fastmedia_bot.py
+++ fastmedia_bot.py
@@ -1,10 +1,14 @@
 import os
-from telegram.ext import ApplicationBuilder
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+async def start(update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("FastMedia Bot aktif di server!")
+
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-print("FastMedia Bot running...")
+app.add_handler(CommandHandler("start", start))
 
+print("FastMedia Bot running...")
 app.run_polling()
